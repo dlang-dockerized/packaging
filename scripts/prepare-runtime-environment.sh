2c9119a85ba795e01556358acd8706ea2e7e05ed
Elias Batek <desisma@heidel.beer>
2024-06-25 22:16:52 +0200

    Move runtime-env preparation logic into script

---
+++ scripts/prepare-runtime-environment.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+if [ -z "$DISTRO" ]; then
+	. /etc/os-release
+	DISTRO=$ID
+fi
+
+echo "System is '$DISTRO'"
+
+if
+	[ "$DISTRO" = "debian" ] ||
+	[ "$DISTRO" = "ubuntu" ] ||
+	[ "$DISTRO" = "linuxmint" ]
+then
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update
+	apt-get -y install --no-install-recommends build-essential	
+    [ -z "$LDC_BOOT_IMAGE" ] && apt-get -y install --no-install-recommends libconfig++
+	rm -rf /var/lib/apt/lists/*
+else
+    echo 'Unsupported distro.'
+    exit 1
+fi
