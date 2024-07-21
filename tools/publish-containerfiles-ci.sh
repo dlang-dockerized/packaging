6bcab442c921b0edb4776feb780d559cf2181310
Elias Batek <desisma@heidel.beer>
2024-07-21 16:07:35 +0200

    Update folder structure of the published containerfiles
    Implements #15.

--- tools/publish-containerfiles-ci.sh
+++ tools/publish-containerfiles-ci.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 noUntracked() {
-	return $(git -C ./containerfiles ls-files --other --directory --exclude-standard | sed q | wc -l)
+	return $(git -C ~/containerfiles ls-files --other --directory --exclude-standard | sed q | wc -l)
 }
-if git -C ./containerfiles diff --quiet && noUntracked; then
+if git -C ~/containerfiles diff --quiet && noUntracked; then
 	echo "Up-to-date."
 	exit 0
 fi
 set -e
-git -C ./containerfiles add -A
-git -C ./containerfiles commit -m "Update containerfiles to dlang-dockerized/packaging@${GITHUB_SHA}"
-git -C ./containerfiles push
+git -C ~/containerfiles add -A
+git -C ~/containerfiles commit -m "Update containerfiles to dlang-dockerized/packaging@${GITHUB_SHA}"
+git -C ~/containerfiles push
